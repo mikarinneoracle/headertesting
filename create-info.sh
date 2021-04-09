3ee380126a5a18bf93e1f986b6fd6ae7e2a7f876
Mika Rinne <mika.rinne@oracle.com>
2021-04-09 13:20:20 +0300

    update

--- create-info.sh
+++
@@ -1,6 +0,0 @@
-../flux create source git headertesting2 \
-  --url=https://github.com/mikarinneoracle/headertesting \
-  --branch=master \
-  --interval=30s \
-  --export > ./clusters/my-cluster/headertesting2-source.yaml
-
--- create-kustomization-info.sh
+++
@@ -1,7 +0,0 @@
-../flux create kustomization headertesting2 \
-  --source=headertesting2 \
-  --path="./kustomize" \
-  --prune=true \
-  --validation=client \
-  --interval=5m \
-  --export > ./clusters/my-cluster/headertesting2-kustomization.yaml
